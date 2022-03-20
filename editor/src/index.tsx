0fb8fe77c959c2a88ec9a9a8ee0e5d3c8b56a04b
Zyriab <flacodevworks@gmail.com>
2022-03-20 19:19:58 +0100

    Renamed files to TS/TSX

--- editor/src/index.tsx
+++ editor/src/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import './index.css';
 
-const theme = createTheme({});
-
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
