b491c814c899c893fa782ea1edac6bb9491ae7d6
Zyriab <flacodevworks@gmail.com>
2022-03-22 23:05:02 +0100

    Translated to TS

--- editor/src/utils/mergeRefs.utils.ts
+++ editor/src/utils/mergeRefs.utils.ts
@@ -1,11 +1,13 @@
+import React from 'react';
+
 /**
  * @see https://www.davedrinks.coffee/how-do-i-use-two-react-refs/
  */
-export function mergeRefs(...refs) {
+export function mergeRefs<T>(...refs: Array<React.MutableRefObject<T> | React.RefCallback<T>>) {
   const filteredRefs = refs.filter(Boolean);
   if (!filteredRefs.length) return null;
   if (filteredRefs.length === 0) return filteredRefs[0];
-  return (inst) => {
+  return (inst: T) => {
     for (const ref of filteredRefs) {
       if (typeof ref === 'function') {
         ref(inst);
