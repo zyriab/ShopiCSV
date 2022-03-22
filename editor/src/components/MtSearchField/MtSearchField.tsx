248ec7f9bf4757b654cc06401cdf7f3ca11cb087
Zyriab <flacodevworks@gmail.com>
2022-03-22 19:59:47 +0100

    Translated to TS

--- editor/src/components/MtSearchField/MtSearchField.tsx
+++ editor/src/components/MtSearchField/MtSearchField.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect } from 'react';
+import React, { useState, useCallback, useMemo, useEffect } from 'react';
 import debounce from 'lodash.debounce';
 import { styled, alpha } from '@mui/material/styles';
 import InputBase from '@mui/material/InputBase';
@@ -50,27 +50,37 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-export function MtSearchField(props) {
-  const { data, filteredDataIds } = props;
+interface AppProps {
+  data: string[][];
+  filteredDataIds: number[];
+}
+
+export function MtSearchField(props: AppProps) {
   const [inputValue, setInputValue] = useState('');
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const searchInput = useCallback(
+    (value: string) => {
+      if (value.trim() !== '') {
+        for (let d of props.data) {
+          let i: number;
+          // TODO: set index based on selected filter ? (something like that?)
+          i = d.findIndex((e) => e[5].trim().includes(value.trim()));
+          if (i !== -1) props.filteredDataIds.push(i);
+        }
+      }
+    },
+    [props.data, props.filteredDataIds]
+  );
+
   const debSearch = useMemo(
     () => debounce(searchInput, 600),
-    [filteredDataIds, data]
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [props.filteredDataIds, props.data, searchInput]
   );
-  function searchInput(value) {
-    const dt = data.map((e, i) => {
-      return { data: e.data[5], id: i };
-    });
-    if (value.trim() !== '') {
-      filteredDataIds(dt.filter((e) => e.data.trim().includes(value.trim())));
-    } else {
-      filteredDataIds(dt);
-    }
-  }
 
-  function handleChange(e) {
+  function handleChange(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
     setInputValue(e.target.value);
     debSearch(e.target.value);
   }
