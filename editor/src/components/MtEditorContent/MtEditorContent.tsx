6bff3d2e13ffcd8867bf1dcfa6db143575b8e2a9
Zyriab <flacodevworks@gmail.com>
2022-03-22 23:41:06 +0100

    removed redundant null checks

--- editor/src/components/MtEditorContent/MtEditorContent.tsx
+++ editor/src/components/MtEditorContent/MtEditorContent.tsx
@@ -31,244 +31,252 @@ export type MtEditorContentElement = {
 };
 
 // TODO: display the current fields page when changing rows display num
-export const MtEditorContent = forwardRef<MtEditorContentElement, AppProps>((props: AppProps, ref) => {
-  const [isReady, setIsReady] = useState(false);
-  const {
-    previousPageNum,
-    selectedPage,
-    resetPagination,
-    maxElementsPerPage,
-    handleElementsPerPageChange,
-    pageContent,
-    setPageContent,
-    GoToPageField,
-    goToPageFieldProps,
-    ChangePageButtons,
-    changePageButtonsProps,
-  } = usePagination(props.data.length, 'rowsNumber');
+export const MtEditorContent = forwardRef<MtEditorContentElement, AppProps>(
+  (props: AppProps, ref) => {
+    const [isReady, setIsReady] = useState(false);
+    const {
+      previousPageNum,
+      selectedPage,
+      resetPagination,
+      maxElementsPerPage,
+      handleElementsPerPageChange,
+      pageContent,
+      setPageContent,
+      GoToPageField,
+      goToPageFieldProps,
+      ChangePageButtons,
+      changePageButtonsProps,
+    } = usePagination(props.data.length, 'rowsNumber');
 
-  useImperativeHandle(ref, () => ({
-    resetPagination,
-  }));
+    useImperativeHandle(ref, () => ({
+      resetPagination,
+    }));
 
-  // TODO: OPTI: 'data' shallowly changes, making React call this effect a second time on page load
-  useEffect(() => {
-    if (props.data.length === 0) setPageContent([]);
-    if (props.data.length > 0 && !props.isLoading) {
-      const fieldNames = [
-        'Type',
-        'Identification',
-        'Field',
-        'Locale',
-        'Status',
-        'Default content',
-        'Translated content',
-      ];
+    // TODO: OPTI: 'data' shallowly changes, making React call this effect a second time on page load
+    useEffect(() => {
+      if (props.data.length === 0) setPageContent([]);
+      if (props.data.length > 0 && !props.isLoading) {
+        const fieldNames = [
+          'Type',
+          'Identification',
+          'Field',
+          'Locale',
+          'Status',
+          'Default content',
+          'Translated content',
+        ];
 
-      async function displayPage() {
-        setIsReady(false);
-        props.setIsLoading(true);
+        async function displayPage() {
+          setIsReady(false);
+          props.setIsLoading(true);
 
-        // if user has gone to another page, save previous page
-        if (selectedPage !== previousPageNum.current) props.onSave();
+          // if user has gone to another page, save previous page
+          if (selectedPage !== previousPageNum.current) props.onSave();
 
-        const content: JSX.Element[] = [];
-        props.renderedFields.current = [];
+          const content: JSX.Element[] = [];
+          props.renderedFields.current = [];
 
-        function setRow(i: number) {
-          function hasHTMLInRow(row: string[]) {
-            return (
-              new RegExp(
-                '<(br|basefont|hr|input|source|frame|param|area|meta|!--|col|link|option|base|img|wbr|!DOCTYPE).*?>|<(a|abbr|acronym|address|applet|article|aside|audio|b|bdi|bdo|big|blockquote|body|button|canvas|caption|center|cite|code|colgroup|command|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frameset|head|header|hgroup|h1|h2|h3|h4|h5|h6|html|i|iframe|ins|kbd|keygen|label|legend|li|map|mark|menu|meter|nav|noframes|noscript|object|ol|optgroup|output|p|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video).*?<\\/\\2>'
-              ).test(row[5]) ||
-              new RegExp(
-                '<(br|basefont|hr|input|source|frame|param|area|meta|!--|col|link|option|base|img|wbr|!DOCTYPE).*?>|<(a|abbr|acronym|address|applet|article|aside|audio|b|bdi|bdo|big|blockquote|body|button|canvas|caption|center|cite|code|colgroup|command|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frameset|head|header|hgroup|h1|h2|h3|h4|h5|h6|html|i|iframe|ins|kbd|keygen|label|legend|li|map|mark|menu|meter|nav|noframes|noscript|object|ol|optgroup|output|p|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video).*?<\\/\\2>'
-              ).test(row[6]) ||
-              row[0].includes('SMS_TEMPLATE') ||
-              row[2].includes('BODY_HTML') ||
-              false
+          function setRow(i: number) {
+            function hasHTMLInRow(row: string[]) {
+              return (
+                new RegExp(
+                  '<(br|basefont|hr|input|source|frame|param|area|meta|!--|col|link|option|base|img|wbr|!DOCTYPE).*?>|<(a|abbr|acronym|address|applet|article|aside|audio|b|bdi|bdo|big|blockquote|body|button|canvas|caption|center|cite|code|colgroup|command|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frameset|head|header|hgroup|h1|h2|h3|h4|h5|h6|html|i|iframe|ins|kbd|keygen|label|legend|li|map|mark|menu|meter|nav|noframes|noscript|object|ol|optgroup|output|p|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video).*?<\\/\\2>'
+                ).test(row[5]) ||
+                new RegExp(
+                  '<(br|basefont|hr|input|source|frame|param|area|meta|!--|col|link|option|base|img|wbr|!DOCTYPE).*?>|<(a|abbr|acronym|address|applet|article|aside|audio|b|bdi|bdo|big|blockquote|body|button|canvas|caption|center|cite|code|colgroup|command|datalist|dd|del|details|dfn|dialog|dir|div|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frameset|head|header|hgroup|h1|h2|h3|h4|h5|h6|html|i|iframe|ins|kbd|keygen|label|legend|li|map|mark|menu|meter|nav|noframes|noscript|object|ol|optgroup|output|p|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video).*?<\\/\\2>'
+                ).test(row[6]) ||
+                row[0].includes('SMS_TEMPLATE') ||
+                row[2].includes('BODY_HTML') ||
+                false
+              );
+            }
+
+            // i.e: (2 x 5) - (5 - [0, 1, 2, 3, 4])
+            const index = Math.round(
+              selectedPage * maxElementsPerPage - (maxElementsPerPage - (i + 1))
+            );
+
+            if (index >= props.data.length) return;
+
+            let row = props.data[index];
+            const tmp = [];
+
+            // had a bug once, data.length was 9 with empty indexes :/
+            if (row.length > 7) row = row.splice(0, 7);
+
+            if (row.length === 7) {
+              const hasEditor = hasHTMLInRow(row);
+
+              for (let x = 0; x < 7; x++) {
+                let width =
+                  !props.display.includes(5) && !props.display.includes(6)
+                    ? true
+                    : 1;
+                if (props.display.length !== 7 && (x === 5 || x === 6)) {
+                  // code editor does not resize well with xs={true}
+                  if (hasEditor) {
+                    if (
+                      (!props.display.includes(5) &&
+                        props.display.includes(6)) ||
+                      (props.display.includes(5) && !props.display.includes(6))
+                    ) {
+                      // other fields are of size 1 if [5]/[6] is present
+                      // Here we substract the number of displayed fields from the number of columns (7)
+                      // -1 is because [5]/[6] is not of size 1 and we want to make this calculation
+                      // based on the OTHER fields ;)
+                      // i.e: 7 col - (3-1) fields = size of 5 (+2 fields of size 1 = 7 columns taken 🤓)
+                      width = 7 - (props.display.length - 1);
+                    } else {
+                      if (props.display.length === 2) width = 3.5;
+                      else if (props.display.length === 4) width = 2.5;
+                      else if (props.display.length === 6) width = 1.5;
+                      else width = Math.ceil(7 / props.display.length);
+                    }
+                  } else width = true;
+                }
+
+                if (props.display.includes(x)) {
+                  props.renderedFields.current.push(
+                    createRef<MtFieldElement>()
+                  );
+                  const key = `${index}-${x}`;
+                  const isCode = (x === 5 || x === 6) && hasEditor;
+                  const fieldRef: React.RefObject<MtFieldElement> =
+                    props.renderedFields.current[
+                      props.renderedFields.current.length - 1
+                    ];
+
+                  tmp.push(
+                    <Grid
+                      key={`grid-${x}`}
+                      xs={width}
+                      alignItems="stretch"
+                      item>
+                      <MtEditorField
+                        ref={fieldRef}
+                        key={key}
+                        kid={key}
+                        code={isCode}
+                        label={fieldNames[x]}
+                        fullWidth={width === true}
+                        value={row[x]}
+                      />
+                    </Grid>
+                  );
+
+                  // setTimeout here is because there is a small latency before the ref is set
+                  setTimeout(() => {
+                    if (!fieldRef.current?.isCode()) {
+                      const el =
+                        fieldRef.current?.getElement() as HTMLInputElement;
+                      el.style.height = '100%';
+                      (el.children[1] as HTMLInputElement).style.height =
+                        '100%';
+                    }
+                  }, 0);
+                }
+              }
+            }
+            return tmp;
+          }
+
+          for (let i = 0; i < maxElementsPerPage; i++) {
+            content.push(
+              <Grid
+                key={i}
+                container
+                item
+                direction="row"
+                justifyContent="center"
+                spacing={1}>
+                {setRow(i)}
+              </Grid>
             );
           }
 
-          // i.e: (2 x 5) - (5 - [0, 1, 2, 3, 4])
-          const index = Math.round(
-            selectedPage * maxElementsPerPage - (maxElementsPerPage - (i + 1))
-          );
-
-          if (index >= props.data.length) return;
-
-          let row = props.data[index];
-          const tmp = [];
-
-          // had a bug once, data.length was 9 with empty indexes :/
-          if (row.length > 7) row = row.splice(0, 7);
-
-          if (row.length === 7) {
-            const hasEditor = hasHTMLInRow(row);
-
-            for (let x = 0; x < 7; x++) {
-              let width =
-                !props.display.includes(5) && !props.display.includes(6)
-                  ? true
-                  : 1;
-              if (props.display.length !== 7 && (x === 5 || x === 6)) {
-                // code editor does not resize well with xs={true}
-                if (hasEditor) {
-                  if (
-                    (!props.display.includes(5) && props.display.includes(6)) ||
-                    (props.display.includes(5) && !props.display.includes(6))
-                  ) {
-                    // other fields are of size 1 if [5]/[6] is present
-                    // Here we substract the number of displayed fields from the number of columns (7)
-                    // -1 is because [5]/[6] is not of size 1 and we want to make this calculation
-                    // based on the OTHER fields ;)
-                    // i.e: 7 col - (3-1) fields = size of 5 (+2 fields of size 1 = 7 columns taken 🤓)
-                    width = 7 - (props.display.length - 1);
-                  } else {
-                    if (props.display.length === 2) width = 3.5;
-                    else if (props.display.length === 4) width = 2.5;
-                    else if (props.display.length === 6) width = 1.5;
-                    else width = Math.ceil(7 / props.display.length);
-                  }
-                } else width = true;
-              }
-
-              if (props.display.includes(x)) {
-                props.renderedFields.current.push(createRef<MtFieldElement>());
-                const key = `${index}-${x}`;
-                const isCode = (x === 5 || x === 6) && hasEditor;
-                const fieldRef: React.RefObject<MtFieldElement> =
-                  props.renderedFields.current[
-                    props.renderedFields.current.length - 1
-                  ];
-
-                tmp.push(
-                  <Grid key={`grid-${x}`} xs={width} alignItems="stretch" item>
-                    <MtEditorField
-                      ref={fieldRef}
-                      key={key}
-                      kid={key}
-                      code={isCode}
-                      label={fieldNames[x]}
-                      fullWidth={width === true}
-                      value={row[x]}
-                    />
-                  </Grid>
-                );
-
-                // setTimeout here is because there is a small latency before the ref is set
-                setTimeout(() => {
-                  if (fieldRef?.current) {
-                    const el =
-                      fieldRef.current.getElement() as HTMLInputElement;
-                    el.style.height = '100%';
-                    (el.children[1] as HTMLInputElement).style.height = '100%';
-                  } else {
-                    console.log('fields not set, could not resize them');
-                  }
-                }, 0);
-              }
-            }
-          }
-          return tmp;
+          setPageContent(content);
+          props.setIsLoading(false);
+          setIsReady(true);
         }
+        displayPage();
+      }
+    }, [
+      maxElementsPerPage,
+      previousPageNum,
+      props,
+      selectedPage,
+      setPageContent,
+    ]);
 
-        for (let i = 0; i < maxElementsPerPage; i++) {
-          content.push(
+    return (
+      <>
+        <Backdrop
+          sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={props.isLoading}>
+          <MtSpinner />
+        </Backdrop>
+        <MtBackToTopBtn />
+        {pageContent.length > 0 && isReady ? (
+          <Grid
+            sx={{
+              marginTop: '1rem',
+              marginBottom: '.5rem',
+              paddingLeft: '.5rem',
+              paddingRight: '.5rem',
+            }}
+            columns={7}
+            container
+            direction="column"
+            justifyContent="flext-start"
+            spacing={1}>
+            {pageContent}
             <Grid
-              key={i}
+              xs
+              sx={{ paddingTop: '1ch' }}
               container
               item
               direction="row"
-              justifyContent="center"
-              spacing={1}>
-              {setRow(i)}
+              justifyContent="flex-end"
+              alignItems="center"
+              spacing={2}>
+              <Grid item>
+                <MtRowsDisplayControl
+                  maxRowDisplay={maxElementsPerPage}
+                  handleRowsDisplayChange={handleElementsPerPageChange}
+                />
+              </Grid>
+              <Grid sm={1} lg item />
+              <Grid xs item container direction="row" justifyContent="flex-end">
+                <Grid xs={1.5} item>
+                  <GoToPageField {...goToPageFieldProps} />
+                </Grid>
+                <Grid xs={4} item sx={{ paddingTop: '1rem' }}>
+                  <ChangePageButtons {...changePageButtonsProps} />
+                </Grid>
+              </Grid>
             </Grid>
-          );
-        }
-
-        setPageContent(content);
-        props.setIsLoading(false);
-        setIsReady(true);
-      }
-      displayPage();
-    }
-  }, [
-    maxElementsPerPage,
-    previousPageNum,
-    props,
-    selectedPage,
-    setPageContent,
-  ]);
-
-  return (
-    <>
-      <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={props.isLoading}>
-        <MtSpinner />
-      </Backdrop>
-      <MtBackToTopBtn />
-      {pageContent.length > 0 && isReady ? (
-        <Grid
-          sx={{
-            marginTop: '1rem',
-            marginBottom: '.5rem',
-            paddingLeft: '.5rem',
-            paddingRight: '.5rem',
-          }}
-          columns={7}
-          container
-          direction="column"
-          justifyContent="flext-start"
-          spacing={1}>
-          {pageContent}
+          </Grid>
+        ) : (
           <Grid
-            xs
-            sx={{ paddingTop: '1ch' }}
             container
-            item
-            direction="row"
-            justifyContent="flex-end"
-            alignItems="center"
-            spacing={2}>
-            <Grid item>
-              <MtRowsDisplayControl
-                maxRowDisplay={maxElementsPerPage}
-                handleRowsDisplayChange={handleElementsPerPageChange}
+            sx={{
+              hreight: '250px',
+              marginTop: '1rem',
+              paddingLeft: '.5rem',
+              paddingRight: '.5rem',
+            }}
+            justifyContent="center"
+            alignItems="stretch">
+            <Grid xs={6} sx={{ marginTop: '15vh' }} item>
+              <MtDropZone
+                text="Drag and drop your CSV file or click"
+                acceptedFiles="text/csv"
+                onChange={props.onUpload}
               />
             </Grid>
-            <Grid sm={1} lg item />
-            <Grid xs item container direction="row" justifyContent="flex-end">
-              <Grid xs={1.5} item>
-                <GoToPageField {...goToPageFieldProps} />
-              </Grid>
-              <Grid xs={4} item sx={{ paddingTop: '1rem' }}>
-                <ChangePageButtons {...changePageButtonsProps} />
-              </Grid>
-            </Grid>
           </Grid>
-        </Grid>
-      ) : (
-        <Grid
-          container
-          sx={{
-            hreight: '250px',
-            marginTop: '1rem',
-            paddingLeft: '.5rem',
-            paddingRight: '.5rem',
-          }}
-          justifyContent="center"
-          alignItems="stretch">
-          <Grid xs={6} sx={{ marginTop: '15vh' }} item>
-            <MtDropZone
-              text="Drag and drop your CSV file or click"
-              acceptedFiles="text/csv"
-              onChange={props.onUpload}
-            />
-          </Grid>
-        </Grid>
-      )}
-    </>
-  );
-});
+        )}
+      </>
+    );
+  }
+);
