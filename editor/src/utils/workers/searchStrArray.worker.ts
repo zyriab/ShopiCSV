b4545089e4c16c2b83f75f6b2c9a1c790e151dd5
Zyriab <flacodevworks@gmail.com>
2022-03-24 15:36:37 +0100

    Moved function to dedicated file + added worker-loader logic

--- editor/src/utils/workers/searchStrArray.worker.ts
+++ editor/src/utils/workers/searchStrArray.worker.ts
@@ -1,46 +1,14 @@
-import { rowData } from '../../definitions/definitions';
+import {searchStrArray} from '../searchStrArray';
 
-export function searchStrArray(searchValue: string, data: string[] | rowData[], searchIndex: number) {
-  let ids: number[] = [];
-  let i: number;
-  if (searchValue.trim() !== '') {
-    if ((data[0] as rowData)?.data) {
-      ids = (data as rowData[])
-        .filter((e: rowData) =>
-          e.data[searchIndex]
-            .trim()
-            .toLowerCase()
-            .normalize('NFD')
-            .replace(/\p{Diacritic}/gu, '')
-            .includes(
-              searchValue
-                .trim()
-                .toLowerCase()
-                .normalize('NFD')
-                .replace(/\p{Diacritic}/gu, '')
-            )
-        )
-        .map((e: rowData) => e.id);
-    } else {
-      for (const d of data) {
-        i =
-          (d as string)
-            .toLowerCase()
-            .normalize('NFD')
-            .replace(/\p{Diacritic}/gu, '') ===
-          searchValue
-            .trim()
-            .toLowerCase()
-            .normalize('NFD')
-            .replace(/\p{Diacritic}/gu, '')
-            ? (data as string[]).indexOf(d as string)
-            : -1;
-        if (i !== -1) ids.push(i);
-      }
-    }
-    // Since the index starts on 1 in MtEditorContent we need to start with 0 which will be skipped
-    // i.e.: index 0 is 'Type', 'Identification', 'Field', etc on translation CSV
-    ids.unshift(0);
-  } else ids = [];
-  return ids;
-}
+// eslint-disable-next-line no-restricted-globals
+const ctx: Worker = self as any;
+let result: number[] = [];
+
+ctx.addEventListener('message', (event) => {
+  result = searchStrArray(
+    event.data.searchValue,
+    event.data.data,
+    event.data.searchIndex
+  );
+  ctx.postMessage(result);
+});
