87e5d2b6f7f8dfc38fa1115ca03899a7af2d20a0
Zyriab <flacodevworks@gmail.com>
2022-03-25 20:21:50 +0100

    Added definition for filter feature + fixed typo

--- editor/src/definitions/definitions.d.ts
+++ editor/src/definitions/definitions.d.ts
@@ -1,1 +1,26 @@
-export type rowData = { data: string[]; id: number };
+export type RowData = { id: number; data: string[] };
+
+export type TranslatableResourceType =
+  | 'COLLECTION'
+  | 'DELIVERY_METHOD_DEFINITION'
+  | 'EMAIL_TEMPLATE'
+  | 'SMS_TEMPLATE'
+  | 'LINK'
+  | 'METAFIELD'
+  | 'ONLINE_STORE_ARTICLE'
+  | 'ONLINE_STORE_BLOG'
+  | 'ONLINE_STORE_MENU'
+  | 'ONLINE_STORE_PAGE'
+  | 'ONLINE_STORE_THEME'
+  | 'PACKING_SLIP_TEMPLATE'
+  | 'PAYMENT_GATEWAY'
+  | 'PRODUCT'
+  | 'PRODUCT_OPTION'
+  | 'PRODUCT_VARIANT'
+  | 'SHOP'
+  | 'SHOP_POLICY';
+
+  export interface FilterType {
+    type: TranslatableResourceType;
+    description?: string;
+  }
