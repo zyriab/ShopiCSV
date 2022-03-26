3080db8e9a5db89b83718c87f57bfea95dc1b5c3
Zyriab <flacodevworks@gmail.com>
2022-03-26 22:25:58 +0100

    Added auth and routing

--- editor/src/index.tsx
+++ editor/src/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { AuthProvider } from './auth/AuthProvider';
+import { BrowserRouter } from 'react-router-dom';
 import './utils/translations/translations.ts';
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
