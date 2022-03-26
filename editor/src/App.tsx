e35a6825cfbf73902b86ee516f800a438a04a7fe
Zyriab <flacodevworks@gmail.com>
2022-03-26 23:46:17 +0100

    Removed useless dependency

--- editor/src/App.tsx
+++ editor/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as dotenv from 'dotenv';
 import { ThemeProvider } from '@mui/material/styles';
 import { ConfirmProvider } from 'material-ui-confirm';
 import {MtRouter} from './components/MtRouter/MtRouter';
@@ -12,7 +11,6 @@ import { MtFooter } from './components/MtFooter/MtFooter';
 import './App.css';
 
 function App() {
-  dotenv.config();
   const darkMode = useDarkMode();
   return (
     <ThemeProvider theme={darkMode.theme}>
