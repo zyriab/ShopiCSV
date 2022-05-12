73a0bc25e735ce94d5fddddde2f80eb86941ddaa
Zyriab <flacodevworks@gmail.com>
2022-05-13 00:21:22 +0200

    Chore: started to migrate ui

--- editor/src/App.tsx
+++ editor/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { AppProvider as PolarisProvider } from '@shopify/polaris';
+import '@shopify/polaris/build/esm/styles.css';
 import { ThemeProvider } from '@mui/material/styles';
 import { ConfirmProvider } from 'material-ui-confirm';
-import {MtRouter} from './components/MtRouter/MtRouter';
+import { MtRouter } from './components/MtRouter/MtRouter';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import { useDarkMode } from './utils/hooks/useDarkMode';
@@ -9,21 +11,24 @@ import { MtNavBar } from './components/MtNavBar/MtNavBar';
 import { MtFooter } from './components/MtFooter/MtFooter';
 
 import './App.css';
+import { getPolarisLocale } from './utils/tools/getPolarisLocale.utils';
 
 function App() {
   const darkMode = useDarkMode();
   return (
-    <ThemeProvider theme={darkMode.theme}>
-      <ConfirmProvider>
-        <Paper>
-          <MtNavBar onModeChange={darkMode.setIsDark} />
-          <Box sx={{ minHeight: '600px' }}>
-            <MtRouter />
-          </Box>
-          <MtFooter />
-        </Paper>
-      </ConfirmProvider>
-    </ThemeProvider>
+    <PolarisProvider i18n={getPolarisLocale as any}>
+      <ThemeProvider theme={darkMode.theme}>
+        <ConfirmProvider>
+          <Paper>
+            <MtNavBar onModeChange={darkMode.setIsDark} />
+            <Box sx={{ minHeight: '600px' }}>
+              <MtRouter />
+            </Box>
+            <MtFooter />
+          </Paper>
+        </ConfirmProvider>
+      </ThemeProvider>
+    </PolarisProvider>
   );
 }
 
