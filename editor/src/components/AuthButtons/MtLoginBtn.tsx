0be5dbe9f74934716189dee5bc2bf4a7414f7c2d
Zyriab <flacodevworks@gmail.com>
2022-05-13 00:24:02 +0200

    Chore: migrated ui

--- editor/src/components/AuthButtons/MtLoginBtn.tsx
+++ editor/src/components/AuthButtons/MtLoginBtn.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import Button from '@mui/material/Button';
+import { Button } from '@shopify/polaris';
 
 export const MtLoginBtn = () => {
   const { loginWithPopup } = useAuth0();
   return (
-    <Button
-      variant="contained"
-      color="success"
-      onClick={() => loginWithPopup()}>
+    <Button primary onClick={() => loginWithPopup()}>
       Log In
     </Button>
   );
--- editor/src/components/AuthButtons/MtLogoutBtn.tsx
+++ editor/src/components/AuthButtons/MtLogoutBtn.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import Button from '@mui/material/Button';
+import { Button } from '@shopify/polaris';
 
 export const MtLogoutBtn = () => {
   const { logout } = useAuth0();
   return (
     <Button
-      variant="contained"
-      color="error"
+      destructive
       onClick={() =>
         logout({
           returnTo: window.location.origin,
