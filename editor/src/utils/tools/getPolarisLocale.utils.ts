609ead2666f9118cae6197f21eab32aeafe79c06
Zyriab <flacodevworks@gmail.com>
2022-05-12 23:59:11 +0200

    Create getPolarisLocale.utils.ts

---
+++ editor/src/utils/tools/getPolarisLocale.utils.ts
@@ -0,0 +1,14 @@
+import i18next from 'i18next';
+import fr from '@shopify/polaris/locales/fr.json';
+import en from '@shopify/polaris/locales/en.json';
+
+export function getPolarisLocale() {
+    switch (i18next.resolvedLanguage) {
+        case 'en-US':
+            return en;
+        case 'fr':
+            return fr;
+        default:
+            return en;
+    }
+}
