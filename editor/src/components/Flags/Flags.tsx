b711975fe8287ca321d1c66b22b570ced6c5993a
Zyriab <flacodevworks@gmail.com>
2022-05-16 20:09:51 +0200

    Added direct svg exports through <img/> + minor fn name fix

--- editor/src/components/Flags/Flags.tsx
+++ editor/src/components/Flags/Flags.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { Icon } from '@shopify/polaris';
-import enFlagSvg from '../../utils/flags/en';
-import frFlagSvg from '../../utils/flags/fr';
 
-export function flagEN() {
-  return <Icon source={enFlagSvg} />;
+export function FlagEN() {
+  return <img src="images/icons/flagEN.svg" alt="" />;
 }
-export function flagFR() {
-  return <Icon source={frFlagSvg} />;
+export function FlagFR() {
+  return <img src="images/icons/flagFR.svg" alt=""/>;
 }
