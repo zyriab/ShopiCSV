e650718add5175b853937852ed0c6e73b2727fc4
Zyriab <flacodevworks@gmail.com>
2022-05-16 11:56:38 +0200

    Created SVG wrappers

---
+++ editor/src/utils/flags/en.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+export default function en() {
+  return (
+    <svg
+      width="20"
+      height="16"
+      viewBox="0 0 20 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <rect width="20" height="16" fill="black" />
+      <rect x="1" y="1" width="18" height="14" fill="#002395" />
+      <rect
+        x="1"
+        y="2.59727"
+        width="2"
+        height="20.5102"
+        transform="rotate(-53 1 2.59727)"
+        fill="white"
+      />
+      <rect
+        x="1.32498"
+        y="2.24489"
+        width="1"
+        height="20.4226"
+        transform="rotate(-53 1.32498 2.24489)"
+        fill="#ED2939"
+      />
+      <rect
+        x="17.3801"
+        y="1"
+        width="2"
+        height="20.5102"
+        transform="rotate(53 17.3801 1)"
+        fill="white"
+      />
+      <rect
+        x="17.6845"
+        y="1.42485"
+        width="1"
+        height="20.357"
+        transform="rotate(53 17.6845 1.42485)"
+        fill="#ED2939"
+      />
+      <rect x="8" y="1" width="3" height="14" fill="white" />
+      <rect
+        x="1"
+        y="10"
+        width="3"
+        height="18"
+        transform="rotate(-90 1 10)"
+        fill="white"
+      />
+      <rect x="9" y="1" width="1" height="14" fill="#ED2939" />
+      <rect
+        x="1"
+        y="9"
+        width="1"
+        height="18"
+        transform="rotate(-90 1 9)"
+        fill="#ED2939"
+      />
+    </svg>
+  );
+}
---
+++ editor/src/utils/flags/fr.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function fr() {
+  return (
+    <svg
+      width="20"
+      height="16"
+      viewBox="0 0 20 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <rect width="20" height="16" fill="black" />
+      <rect x="1" y="1" width="18" height="14" fill="#ED2939" />
+      <rect x="7" y="1" width="12" height="14" fill="white" />
+      <rect x="13" y="1" width="6" height="14" fill="#002395" />
+    </svg>
+  );
+}
