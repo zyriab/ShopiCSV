f728a889185bef1614819bb2750fa280580d19d4
Zyriab <flacodevworks@gmail.com>
2022-05-16 20:12:44 +0200

    Improved UI/UX/Design

--- editor/src/components/MtDropZone/MtDropZone.tsx
+++ editor/src/components/MtDropZone/MtDropZone.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { DropZone } from '@shopify/polaris';
+import { DropZone, Button, Stack, TextStyle, Card } from '@shopify/polaris';
 
 interface MtDropZoneProps {
   onUpload: (
@@ -10,19 +10,34 @@ interface MtDropZoneProps {
   ) => Promise<void>;
 }
 
+// TODO: implement error handling (customValidator + banner w/ details) @see: https://polaris.shopify.com/components/actions/drop-zone#section-content-guidelines
 export function MtDropZone(props: MtDropZoneProps) {
   const { t } = useTranslation();
-
   return (
-    <DropZone
-      dropOnPage
-      accept="text/csv"
-      overlayText={t('DropZone.message')}
-      allowMultiple={false}
-      onDrop={(files, accepted, rejected) =>
-        props.onUpload(files, accepted, rejected)
-      }>
-      <DropZone.FileUpload actionTitle={t('DropZone.message')} />
-    </DropZone>
+    <div>
+      <Card sectioned>
+        <DropZone
+          dropOnPage
+          accept="text/csv"
+          overlayText={t('DropZone.message')}
+          allowMultiple={false}
+          onDrop={(files, accepted, rejected) =>
+            props.onUpload(files, accepted, rejected)
+          }>
+          <Stack vertical spacing="loose">
+            <div />
+            <Stack vertical alignment="center" spacing="extraTight">
+              <img src="images/icons/UploadSpot.svg" alt="Upload a file" />
+              <Button>{t('DropZone.button')}</Button>
+              <div />
+              <div />
+              <div />
+              <TextStyle variation="subdued">{t('DropZone.message')}</TextStyle>
+            </Stack>
+            <div />
+          </Stack>
+        </DropZone>
+      </Card>
+    </div>
   );
 }
