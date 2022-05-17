d55ec2ca72864b64fe73f1e9e799bad1200e0667
Zyriab <flacodevworks@gmail.com>
2022-05-17 14:45:12 +0200

    Chore: started migrating UI + optimized function usage

--- editor/src/utils/hooks/useDarkMode.tsx
+++ editor/src/utils/hooks/useDarkMode.tsx
@@ -1,8 +1,8 @@
 import {useEffect, useMemo, useState} from 'react';
-import { createTheme } from '@mui/material/styles';
 import store from 'store2';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import { darkTheme, lightTheme } from '../../themes/themes';
+// import { createTheme } from '@mui/material/styles';
+// import { darkTheme, lightTheme } from '../../themes/themes';
 
 // FIXME: sometimes store.get doesn't get it right (too soon?)
 export function useDarkMode() {
@@ -11,17 +11,24 @@ export function useDarkMode() {
     store.get('themeMode') === 'dark' || prefersDarkMode || false
   );
 
-  const theme = useMemo(
-    () => createTheme(isDark ? darkTheme : lightTheme),
-    [isDark]
-  );
+  // Used for MUI
+  // const theme = useMemo(
+  //   () => createTheme(isDark ? darkTheme : lightTheme),
+  //   [isDark]
+  // );
 
   useEffect(() => {
     if (prefersDarkMode) store.set('themeMode', 'dark');
+    else store.set('thememode', 'light');
   }, [prefersDarkMode]);
 
+  useEffect(() => {
+    if (isDark) store.set('themeMode', 'dark');
+    else store.set('thememode', 'light');
+  }, [isDark])
+
   return {
-    theme,
+    // theme,
     isDark,
     setIsDark,
   }
