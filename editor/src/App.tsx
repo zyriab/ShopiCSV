4904e0f5667380aba4c1f0f4169e79b8a5d1bc93
Zyriab <flacodevworks@gmail.com>
2022-05-19 17:24:33 +0200

    Chore: Added logo + cleaned up code

--- editor/src/App.tsx
+++ editor/src/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  AppProvider as PolarisProvider,
-  Frame,
-  Navigation,
-} from '@shopify/polaris';
+import { AppProvider as PolarisProvider, Frame } from '@shopify/polaris';
 import { ThemeProvider } from '@mui/material/styles';
 import { ConfirmProvider } from 'material-ui-confirm';
 import { MtRouter } from './components/MtRouter/MtRouter';
@@ -13,19 +9,26 @@ import useDarkMode from './utils/hooks/useDarkMode';
 import { MtFooter } from './components/MtFooter/MtFooter';
 import MtNavBar from './components/MtNavBar/MtNavBar';
 
-import { HomeMinor, LanguageMinor } from '@shopify/polaris-icons';
-
 import './App.css';
 import '@shopify/polaris/build/esm/styles.css';
 
 function App() {
   const darkMode = useDarkMode();
+
+  const logo = {
+    width: 124,
+    topBarSource:
+      'https://cdn.shopify.com/s/files/1/0446/6937/files/jaded-pixel-logo-color.svg?6215648040070010999',
+    url: 'http://jadedpixel.com',
+    accessibilityLabel: 'Jaded Pixel',
+  };
+
   return (
     <PolarisProvider
       colorScheme={darkMode.isDark ? 'dark' : 'light'}
       i18n={getPolarisLocale as any}>
       <ThemeProvider theme={darkMode.theme}>
-        <Frame topBar={<MtNavBar onModeChange={darkMode.setIsDark} />}>
+        <Frame logo={logo} topBar={<MtNavBar logo={logo} onThemeChange={darkMode.setIsDark} />}>
           <ConfirmProvider>
             <Box sx={{ minHeight: '600px' }}>
               <MtRouter />
