635c69003ed5a9cce6c617ddc84d1bbe82d011bb
Zyriab <flacodevworks@gmail.com>
2022-05-24 00:44:15 +0200

    chore: Migrated UI to Polaris

--- editor/src/components/MtRowsDisplayControl/MtRowsDisplayControl.tsx
+++ editor/src/components/MtRowsDisplayControl/MtRowsDisplayControl.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import { Stack, Button, ButtonGroup, TextStyle } from '@shopify/polaris';
 
 interface AppProps {
   maxRowDisplay: number;
@@ -11,44 +9,29 @@ interface AppProps {
 
 export function MtRowsDisplayControl(props: AppProps) {
   const { t } = useTranslation();
+
+  const BTN_VALUES = [2, 4, 8, 16];
+
   return (
-    <Grid xs container item alignItems="center">
-      <Grid item>
-        <Typography
-          paragraph
-          component="p"
-          sx={{ paddingBottom: '.3rem', marginRight: '.5rem' }}>
-          {t('RowDisplayControl.displayPer')}
-        </Typography>
-      </Grid>
-      <Grid item>
-        <Button
-          variant={props.maxRowDisplay === 2 ? 'outlined' : 'text'}
-          onClick={() => props.handleRowsDisplayChange(2)}>
-          2
-        </Button>
-      </Grid>
-      <Grid item>
-        <Button
-          variant={props.maxRowDisplay === 4 ? 'outlined' : 'text'}
-          onClick={() => props.handleRowsDisplayChange(4)}>
-          4
-        </Button>
-      </Grid>
-      <Grid item>
-        <Button
-          variant={props.maxRowDisplay === 8 ? 'outlined' : 'text'}
-          onClick={() => props.handleRowsDisplayChange(8)}>
-          8
-        </Button>
-      </Grid>
-      <Grid item>
-        <Button
-          variant={props.maxRowDisplay === 16 ? 'outlined' : 'text'}
-          onClick={() => props.handleRowsDisplayChange(16)}>
-          16
-        </Button>
-      </Grid>
-    </Grid>
+    <Stack>
+      <TextStyle variation="subdued">
+        {t('RowDisplayControl.displayPer')}
+      </TextStyle>
+      <ButtonGroup>
+        {BTN_VALUES.map((v) => (
+          <Button
+            key={v}
+            onClick={() => props.handleRowsDisplayChange(v)}
+            outline={props.maxRowDisplay === v}
+            plain={props.maxRowDisplay !== v}
+            monochrome
+            accessibilityLabel={t('RowDisplayControl.btnA11yLabel', {
+              number: v,
+            })}>
+            {`${v}`}
+          </Button>
+        ))}
+      </ButtonGroup>
+    </Stack>
   );
 }
