cdfd16ce4b2b76239e4ff42675c77577685f0558
Zyriab <flacodevworks@gmail.com>
2022-05-23 17:10:44 +0200

    Chore: changed Kb, Mb, ... to Kib, Mib, ... for correctness

--- editor/src/utils/tools/formatBytes.utils.ts
+++ editor/src/utils/tools/formatBytes.utils.ts
@@ -2,7 +2,7 @@ export function formatBytes(bytes: number, decimals = 2) {
     if (bytes === 0) return '0 Bytes';
     const k = 1024;
     const dm = decimals < 0 ? 0 : decimals;
-    const sizes = ['Bytes', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'];
+    const sizes = ['Bytes', 'Kib', 'Mib', 'Gib', 'Tib', 'Pib', 'Eib', 'Zib', 'Yib'];
     const i = Math.floor(Math.log(bytes) / Math.log(k));
     return `${parseFloat((bytes / k ** i).toFixed(dm))}  ${sizes[i]}`;
 }
