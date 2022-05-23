a7448ae3bfd7b0e13bda00c7d28df8698165f206
Zyriab <flacodevworks@gmail.com>
2022-05-23 23:09:12 +0200

    chore: moved all theming components to dedicated component + moved logo to dedicated file + remove legacy dark mode hook

--- editor/src/App.tsx
+++ editor/src/App.tsx
@@ -1,43 +1,28 @@
 import React from 'react';
-import { AppProvider as PolarisProvider, Frame } from '@shopify/polaris';
-import { ThemeProvider } from '@mui/material/styles';
+import { Frame } from '@shopify/polaris';
 import { ConfirmProvider } from 'material-ui-confirm';
+import MtThemeProvider from './components/MtThemeProvider/MtThemeProvider';
 import { MtRouter } from './components/MtRouter/MtRouter';
 import Box from '@mui/material/Box';
-import { getPolarisLocale } from './utils/tools/getPolarisLocale.utils';
-import useDarkMode from './utils/hooks/useDarkMode';
 import { MtFooter } from './components/MtFooter/MtFooter';
 import MtNavBar from './components/MtNavBar/MtNavBar';
+import logo from './utils/helpers/logo.helper';
 
 import './App.css';
 import '@shopify/polaris/build/esm/styles.css';
 
 function App() {
-  const darkMode = useDarkMode();
-
-  const logo = {
-    width: 124,
-    topBarSource:
-      'https://cdn.shopify.com/s/files/1/0446/6937/files/jaded-pixel-logo-color.svg?6215648040070010999',
-    url: 'http://jadedpixel.com',
-    accessibilityLabel: 'Jaded Pixel',
-  };
-
   return (
-    <PolarisProvider
-      colorScheme={darkMode.isDark ? 'dark' : 'light'}
-      i18n={getPolarisLocale as any}>
-      <ThemeProvider theme={darkMode.theme}>
-        <Frame logo={logo} topBar={<MtNavBar logo={logo} onThemeChange={darkMode.setIsDark} />}>
-          <ConfirmProvider>
-            <Box sx={{ minHeight: '600px' }}>
-              <MtRouter />
-            </Box>
-            <MtFooter />
-          </ConfirmProvider>
-        </Frame>
-      </ThemeProvider>
-    </PolarisProvider>
+    <MtThemeProvider>
+      <Frame logo={logo} topBar={<MtNavBar />}>
+        <ConfirmProvider>
+          <Box sx={{ minHeight: '600px' }}>
+            <MtRouter />
+          </Box>
+          <MtFooter />
+        </ConfirmProvider>
+      </Frame>
+    </MtThemeProvider>
   );
 }
 
