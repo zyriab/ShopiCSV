a6b7ac43285764da16d6d04c12fa40ac73a05808
Zyriab <flacodevworks@gmail.com>
2022-05-23 17:15:09 +0200

    Chore: Added proper theme switc fn + fixed logic + cleaned up code

--- editor/src/utils/hooks/useDarkMode.tsx
+++ editor/src/utils/hooks/useDarkMode.tsx
@@ -7,8 +7,16 @@ import { darkTheme, lightTheme } from '../../themes/themes';
 export default function useDarkMode() {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
   const [isDark, setIsDark] = useState(
-    store.get('themeMode') === 'dark' || prefersDarkMode || false
+    store.get('themeMode') === 'dark' ||
+      (store.get('themeMode') == null && prefersDarkMode) ||
+      false
   );
+
+  function activateDarkTheme(dark: boolean) {
+    if (dark) store.set('themeMode', 'dark');
+    else store.set('themeMode', 'light');
+    setIsDark(dark);
+  }
 
   // Used for MUI
   const theme = useMemo(
@@ -17,18 +25,14 @@ export default function useDarkMode() {
   );
 
   useEffect(() => {
-    if (prefersDarkMode) store.set('themeMode', 'dark');
-    else store.set('themeMode', 'light');
-  }, [prefersDarkMode]);
-
-  useEffect(() => {
     if (isDark) store.set('themeMode', 'dark');
     else store.set('themeMode', 'light');
-  }, [isDark]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return {
     theme,
     isDark,
-    setIsDark,
+    activateDarkTheme,
   };
 }
