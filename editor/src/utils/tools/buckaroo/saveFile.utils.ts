746334d2582f6e0a07019c7d6f05245479ebe128
Zyriab <flacodevworks@gmail.com>
2022-06-14 05:00:37 +0200

    fix: implemented working save

--- editor/src/utils/tools/buckaroo/saveFile.utils.ts
+++ editor/src/utils/tools/buckaroo/saveFile.utils.ts
@@ -9,44 +9,63 @@ interface SaveFileArgs {
   token: string;
 }
 
-interface PreSignedPost {
-  url: string;
-  fields: { [key: string]: string };
-}
-
 export default async function SaveFile(args: SaveFileArgs) {
   try {
-    const query = { ...GetUploadUrlQuery };
-    // const form = new FormData();
+    const form = new FormData();
     const file = args.file.map((e) => e.data);
-    // const data = Papa.unparse(file);
+    const data = Papa.unparse(file);
+    const query = { ...GetUploadUrlQuery };
 
     query.variables.fileName = args.fileName;
     query.variables.path = getDataType();
 
-    // console.dir(query);
-
-    // FIXME: "TypeError: Failed to fetch"
+    // Getting AWS signed POST from Buckaroo
     const res = await window.fetch(
       'https://44kxybpuheewejuvm3lkqsxw3m0zevhn.lambda-url.eu-central-1.on.aws/',
       {
         method: 'POST',
-        body: JSON.stringify({ foo: 'bar' }),
+        body: JSON.stringify(query),
         headers: {
           'Content-Type': 'application/json',
-          Authorization: `Bearer ${args.token}`,
+          // Passing the encrypted tenant to call the proper Auth0 API
+          Authorization: `Bearer ${args.token} ${process.env.REACT_APP_TENANT}`,
         },
       }
     );
 
-    // Object.entries(res.body.fields).forEach(([field, value]) =>
-    //   form.append(field, value)
-    // );
-    // form.append('file', data);
+    if (!res.ok) {
+      throw new Error(
+        `Error while fetching save post. ${res.status}: ${res.statusText}`
+      );
+    }
 
-    // console.log(process.env.REACT_APP_BUCKAROO_URL);
-    console.table(res);
+    const resData = await res.json();
+    const typename = resData.data.getUploadUrl.__typename;
+
+    if (typename === 'SignedPost') {
+      const url: string = resData.data.getUploadUrl.url;
+      const fields: Object = resData.data.getUploadUrl.fields;
+
+      Object.entries(fields).forEach(([field, value]: [string, string]) =>
+        form.append(field, value)
+      );
+      form.append('file', data);
+
+      // Uploading file to the bucket
+      const upRes = await window.fetch(url, {
+        method: 'POST',
+        body: form,
+      });
+
+      if (!upRes.ok) {
+        throw new Error(
+          `Error while posting saved file. ${res.status}: ${res.statusText}`
+        );
+      }
+    } else {
+      throw new Error(resData.data.message);
+    }
   } catch (e) {
-    console.log(e);
+    throw e;
   }
 }
