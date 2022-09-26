2461bf75f9e6e20d650881e9b6ad6f7c7f894fca
Zyriab <flacodevworks@gmail.com>
2022-09-26 17:58:26 +0200

    fix: fixed AWS S3 object name handling (directories)

--- editor/src/utils/tools/demo/saveFileLocally.utils.ts
+++ editor/src/utils/tools/demo/saveFileLocally.utils.ts
@@ -1,5 +1,6 @@
 import store from 'store2';
 import { RowData } from '../../../definitions/custom';
+import formatPath from '../fileExplorer/formatPath.utils';
 
 interface inputArgs {
   content: RowData[];
@@ -12,6 +13,7 @@ export default function saveFileLocally(args: inputArgs) {
   store.remove('fileData');
   store.set('fileData', {
     ...args,
+    name: args.name.split('/').at(-1),
     savedAt: new Date().toLocaleString(),
   });
 }
