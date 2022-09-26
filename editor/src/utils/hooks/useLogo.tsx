02f6ddd39198bf9f9658664b83481b7ea3eec425
Zyriab <flacodevworks@gmail.com>
2022-09-26 17:58:08 +0200

    fix: set proper logos + state

--- editor/src/utils/hooks/useLogo.tsx
+++ editor/src/utils/hooks/useLogo.tsx
@@ -1,34 +1,24 @@
-import { useEffect, useRef, useContext } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import useDetectScreenSize from './useDetectScreenSize';
 import themeContext from '../contexts/theme.context';
 import { navBarLogo, lightBgLogo, darkBgLogo } from '../helpers/logo.helper';
 import { Logo } from '@shopify/polaris/build/ts/latest/src/utilities/frame/types';
 
 export default function useLogo(): Logo {
-  const logo = useRef(navBarLogo);
+  const [logo, setLogo] = useState(navBarLogo);
 
   const { isDesktop } = useDetectScreenSize();
   const { isDark } = useContext(themeContext);
 
-  // TODO: resize SVG file
+  useEffect(() => {
+    if (isDesktop) {
+      setLogo(navBarLogo);
+    } else if (isDark) {
+      setLogo(darkBgLogo);
+    } else if (!isDark) {
+      setLogo(lightBgLogo);
+    }
+  }, [isDark, isDesktop]);
 
-  // useEffect(() => {
-  //   if (isDesktop) {
-  //     logo.current = navBarLogo;
-  //   } else if (!isDesktop && isDark) {
-  //     logo.current = darkBgLogo;
-  //   } else if (!isDesktop && !isDark) {
-  //     logo.current = lightBgLogo;
-  //   }
-  // }, [isDark, isDesktop]);
-
-  logo.current = {
-    width: 124,
-    topBarSource:
-      'https://cdn.shopify.com/s/files/1/0446/6937/files/jaded-pixel-logo-color.svg?6215648040070010999',
-    url: 'http://jadedpixel.com',
-    accessibilityLabel: 'Jaded Pixel',
-  };
-
-  return logo.current;
+  return logo;
 }
