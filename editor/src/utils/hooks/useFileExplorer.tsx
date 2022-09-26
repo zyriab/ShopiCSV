9497169dedc8711b69643391e7e3025b3e4e2c67
Zyriab <flacodevworks@gmail.com>
2022-09-26 18:00:57 +0200

    feature: implemented directory and object opening here + modified mock files path

--- editor/src/utils/hooks/useFileExplorer.tsx
+++ editor/src/utils/hooks/useFileExplorer.tsx
@@ -13,6 +13,8 @@ import { BucketObject } from '../../definitions/mtFileExplorer';
 import { listBucketContent } from '../tools/buckaroo/queries.utils';
 import { BucketObjectInfo, FileInput } from '../../definitions/custom';
 import getDataType from '../tools/getDataType.utils';
+import getPathRelativeName from '../tools/fileExplorer/getPathRelativeName.utils';
+import isDirectory from '../tools/fileExplorer/isDirectory.utils';
 import { mockFileContentV1, mockFileContentV2 } from '../tools/demo/filesContent.utils';
 
 import './MtFileExplorer.css';
@@ -70,6 +72,35 @@ export default function useFileExplorer(props: useFileExplorerProps) {
     await props.onDelete(fileInput);
   }
 
+  function handleOpenDirectory(name: string, path: string[]) {
+    setSelectedFileId(undefined);
+    setPath((current) => [
+      ...current,
+      ...[getPathRelativeName(name, path)],
+    ]);
+  }
+
+  function handleClickObject(id: string) {
+    const doubleClicked = id === selectedFileId;
+
+    if (doubleClicked) {
+      const obj = filesContent.find((x) => x.id === id);
+
+      if (obj == null) {
+        return;
+      }
+
+      const name = `${obj.path}/${obj.name}`
+
+      if (obj && isDirectory(getPathRelativeName(name, path))) {
+        handleOpenDirectory(name, path);
+      }
+      return;
+    }
+
+    setSelectedFileId(id);
+  }
+
   const fetchData = useCallback(async () => {
     if (isMounted.current) {
       setIsFetchingObjects(true);
@@ -77,8 +108,8 @@ export default function useFileExplorer(props: useFileExplorerProps) {
       let files: BucketObject[] = [];
 
       if (process.env.REACT_APP_ENV === 'demo') {
-        const f1v2 = { id: '1', path: '/', content: mockFileContentV2.content, size: 3235620, lastModified: new Date(new Date(2021, 0, 1).getTime() + Math.random() * (new Date().getTime() - new Date(2021, 0, 1).getTime())) }
-        const f1 = { id: '0', name: 'FR-EN store translation.csv', path: '/', content: mockFileContentV1.content, size: 3452341, lastModified: new Date(), versions: [f1v2] }
+        const f1v2 = { id: '1', path: 'My store/', content: mockFileContentV2.content, size: 3235620, lastModified: new Date(new Date(2021, 0, 1).getTime() + Math.random() * (new Date().getTime() - new Date(2021, 0, 1).getTime())) }
+        const f1 = { id: '0', name: 'FR-EN store translation.csv', path: 'My store/', content: mockFileContentV1.content, size: 3452341, lastModified: new Date(), versions: [f1v2] }
 
         files = [f1];
       } else {
@@ -136,6 +167,7 @@ export default function useFileExplorer(props: useFileExplorerProps) {
     path,
     setPath,
     onClickUpload: handleClickUpload,
+    onClickObject: handleClickObject,
   };
 
   const previewCardProps = {
@@ -144,6 +176,7 @@ export default function useFileExplorer(props: useFileExplorerProps) {
     path,
     onLoad: props.onFileLoad,
     onDelete: handleDelete,
+    onOpenDirectory: (name: string) => handleOpenDirectory(name, path),
   };
 
   return {
