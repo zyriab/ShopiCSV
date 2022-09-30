55ffe86771de25d092eb5223fd670e03b788ebb2
Zyriab <flacodevworks@gmail.com>
2022-09-30 08:25:03 +0200

    feature: removed i18n debug on demo env

--- editor/src/utils/translations/translations.ts
+++ editor/src/utils/translations/translations.ts
@@ -17,7 +17,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    debug: process.env.NODE_ENV !== 'production',
+    debug: process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'demo',
     resources,
     fallbackLng: 'en',
     interpolation: {
