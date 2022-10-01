1a537f365178b1f4f76fd66aacc28ce9d75dd698
Zyriab <flacodevworks@gmail.com>
2022-10-01 16:23:47 +0200

    fix: added momentary demo env check

--- editor/src/auth/ProtectedRoute.tsx
+++ editor/src/auth/ProtectedRoute.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { withAuthenticationRequired } from '@auth0/auth0-react';
 
-interface AppProps {
+interface ProtectedRouteProps {
   component: React.ComponentType<object>;
   other?: any[];
 }
 
-export function ProtectedRoute(props: AppProps) {
+export function ProtectedRoute(props: ProtectedRouteProps) {
+  if (process.env.REACT_APP_ENV === 'demo') {
+    return <props.component {...props.other} />;
+  }
+
   const Component = withAuthenticationRequired(props.component);
   return <Component {...props.other} />;
 }
