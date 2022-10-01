e74c6e6d3f3fc93d23bfbd466537fd6cd7140ef3
Zyriab <flacodevworks@gmail.com>
2022-10-01 16:25:56 +0200

    fix: removed home page

--- editor/src/components/MtRouter/MtRouter.tsx
+++ editor/src/components/MtRouter/MtRouter.tsx
@@ -1,13 +1,11 @@
 import React, { Route, Routes, Navigate } from 'react-router-dom';
 import { ProtectedRoute } from '../../auth/ProtectedRoute';
-import HomePage from '../../pages/Home';
 import TranslatorPage from '../../pages/Translator';
 
 export function MtRouter() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route
           path="/translations"
           element={<ProtectedRoute component={TranslatorPage} />}
