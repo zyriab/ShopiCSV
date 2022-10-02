39a792ccbfad16386b008fb08007482aa302a720
Zyriab <flacodevworks@gmail.com>
2022-10-02 11:19:56 +0200

    Create MtUserMenuTooltip.tsx

---
+++ editor/src/components/MtUserMenuTooltip/MtUserMenuTooltip.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Tooltip } from '@shopify/polaris';
+
+interface MtUserMenuTooltipProps {
+  name: string;
+  email: string;
+  isUserMenuOpen: boolean;
+  children: React.ReactNode;
+}
+export function MtUserMenuTooltip(props: MtUserMenuTooltipProps) {
+  return props.isUserMenuOpen ? (
+    <>{props.children}</>
+  ) : (
+    <Tooltip
+      content={<div>
+        {props.name}
+        <br />
+        <small>{props.email}</small>
+      </div>}>
+      {props.children}
+    </Tooltip>
+  );
+}
