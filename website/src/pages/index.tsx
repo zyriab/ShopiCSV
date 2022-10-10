fd74abaaaf7db02c436c078b3714404ac4e957cf
Zyriab <flacodevworks@gmail.com>
2022-10-10 15:42:14 +0200

    feature: added recaptcha provider

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import type { HeadFC } from 'gatsby';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import i18next from 'i18next';
 import NavBar from '../components/NavBar/NavBar';
 import MainContent from '../components/MainContent/MainContent';
 import Footer from '../components/Footer/Footer';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { lightTheme } from '../utils/theme.utils';
 import '../utils/translations/translations';
 
@@ -14,11 +16,15 @@ export default function IndexPage() {
 
   return (
     <main>
-      <ThemeProvider theme={theme}>
-        <NavBar />
-        <MainContent />
-        <Footer />
-      </ThemeProvider>
+      <GoogleReCaptchaProvider
+        reCaptchaKey="6LcUv2kiAAAAAOReArJsxu9AUQbhMeCkPBalSL9b"
+        language={i18next.resolvedLanguage}>
+        <ThemeProvider theme={theme}>
+          <NavBar />
+          <MainContent />
+          <Footer />
+        </ThemeProvider>
+      </GoogleReCaptchaProvider>
     </main>
   );
 }
